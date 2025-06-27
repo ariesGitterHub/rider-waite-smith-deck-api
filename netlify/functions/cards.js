3a0130f4359461ffa18e16801b5ffed287a2b622
ariesGitterHub <149708069+ariesGitterHub@users.noreply.github.com>
2025-06-27 15:36:57 -0400

    testing card.js changes

--- netlify/functions/cards.js
+++ netlify/functions/cards.js
@@ -9,9 +9,19 @@
 
 const cards = require("../../data/tarotDeck");
 
-exports.handler = async (event, context) => {
+// exports.handler = async (event, context) => {
+//   return {
+//     statusCode: 200,
+//     body: JSON.stringify(cards)
+//   };
+// };
+
+exports.handler = async function () {
+  const cards = [
+    /* your tarot cards */
+  ];
   return {
     statusCode: 200,
-    body: JSON.stringify(cards)
+    body: JSON.stringify({ cards }),
   };
 };
