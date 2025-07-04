1f7b0b767202ef481088da665130d2175159a930
ariesGitterHub <149708069+ariesGitterHub@users.noreply.github.com>
2025-07-04 15:32:42 -0400

    test for production with changes to card.js

--- netlify/functions/cards.js
+++ netlify/functions/cards.js
@@ -7,11 +7,41 @@
 //   };
 // };
 
+
+// NOTE: Below code works for development, but fails in production
+// const cards = require("../../data/tarotDeck");
+
+// exports.handler = async (event, context) => {
+//   return {
+//     statusCode: 200,
+//     body: JSON.stringify({ cards }), // <------- wrap in object
+//   };
+// };
+
+// Trying this next: 
 const cards = require("../../data/tarotDeck");
 
 exports.handler = async (event, context) => {
+  // Handle CORS preflight request
+  if (event.httpMethod === "OPTIONS") {
+    return {
+      statusCode: 204,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type",
+      },
+      body: "",
+    };
+  }
+
   return {
     statusCode: 200,
-    body: JSON.stringify({ cards }), // <------- wrap in object
+    headers: {
+      "Access-Control-Allow-Origin": "*", // <-- This fixes CORS error
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ cards }),
   };
 };
+
